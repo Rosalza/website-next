e64770480a797ae81af86a3eea36b3a64bb47860
Rosalza <131552887+Rosalza@users.noreply.github.com>
2023-06-20 19:24:04 +0200

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
         </div>
       
         <div className='flex flex-row justify-between items-center w-full'>
-          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[65px] text-[45px] text-white ss:leading-[100px] leading-[75px]'>
             {lang.lng.hero.headlineWhite1} <br className='sm:block hidden' /> {' '} 
             {lang.lng.hero.headlineWhite2}<br className='sm:block hidden' /> {' '} 
             <span 
