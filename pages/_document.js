26686b572e2b1eadb6ba74988da914b79f25065e
Rosalza <131552887+Rosalza@users.noreply.github.com>
2023-07-02 12:07:31 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Rosalza</title>
+        <meta name="description" content="Rosalza - The reveloution of recruiting">
+      </Head>
       <body>
         <Main />
         <NextScript />
